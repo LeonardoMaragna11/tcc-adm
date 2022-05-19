20bfed8b5a4a655cc5d306826cca0cdbfa582e7d
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2022-05-19 11:45:46 -0300

    commit

--- navbarAction.js
+++ navbarAction.js
@@ -1,8 +1,17 @@
 const btnRes = document.getElementById('btnRes');
 const nav = document.getElementById('navbar');
-
+const section = document.getElementsByTagName('section')[0]
 function menu(){
     nav.classList.toggle('active')
+    if(btnRes.innerHTML == 'Fechar'){
+        btnRes.innerHTML = 'Menu'
+    section.style.opacity = '100%'
+    }else{
+        btnRes.innerHTML = 'Fechar'
+    section.style.opacity = '30%'
+
+    }
+    btnRes.classList.toggle('btnActive')
 }
 
 btnRes.addEventListener('click',menu)
